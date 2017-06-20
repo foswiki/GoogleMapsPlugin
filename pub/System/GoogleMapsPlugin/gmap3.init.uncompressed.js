2fb2eb5e9781e08d78ff343bdd4afda76f3ac508
MichaelDaum <daum@michaeldaumconsulting.com>
2017-06-20 15:31:25 +0200

    Item14397: preparing new release

--- pub/System/GoogleMapsPlugin/gmap3.init.uncompressed.js
+++ pub/System/GoogleMapsPlugin/gmap3.init.uncompressed.js
@@ -6,52 +6,51 @@ jQuery(function($) {
         opts = $this.data();
 
     if (typeof(opts.marker) !== 'undefined') {
-      $.each(opts.marker.values, function(index, item) {
-        opts.marker.events =  {
-          "click": function(marker) {
-            var $this = $(this),
-                infowindow = $this.gmap3({get:{name:"infowindow"}}),
-                content;
-            
-            if (item.address) {
-              content = item.address.replace(/, */g, '<br />');
-      
-              if (infowindow) {
-                infowindow.open($this.gmap3("get"), marker);
-                infowindow.setContent(content);
-              } else {
-                $this.gmap3({
-                  infowindow: {
-                    anchor:marker, 
-                    options:{ content: content }
-                  }
-                });
-              }
+
+      opts.marker.events =  {
+        "click": function(marker, event, context) {
+          var $this = $(this),
+              infowindow = $this.gmap3({get:{name:"infowindow"}}),
+              content;
+          
+          if (context.data) {
+            content = context.data.replace(/, */g, '<br />');
+    
+            if (infowindow) {
+              infowindow.open($this.gmap3("get"), marker);
+              infowindow.setContent(content);
             } else {
               $this.gmap3({
-                getaddress: {
-                  latLng:marker.getPosition(),
-                  callback:function(results){
-                    var content = results && results[0] ? results[0].formatted_address : "no address";
-
-                    if (infowindow){
-                      infowindow.open($this.gmap3("get"), marker);
-                      infowindow.setContent(content);
-                    } else {
-                      $this.gmap3({
-                        infowindow: {
-                          anchor:marker, 
-                          options:{ content: content }
-                        }
-                      });
-                    }
-                  }
+                infowindow: {
+                  anchor:marker, 
+                  options:{ content: content }
                 }
               });
             }
+          } else {
+            $this.gmap3({
+              getaddress: {
+                latLng:marker.getPosition(),
+                callback:function(results){
+                  var content = results && results[0] ? results[0].formatted_address : "no address";
+
+                  if (infowindow){
+                    infowindow.open($this.gmap3("get"), marker);
+                    infowindow.setContent(content);
+                  } else {
+                    $this.gmap3({
+                      infowindow: {
+                        anchor:marker, 
+                        options:{ content: content }
+                      }
+                    });
+                  }
+                }
+              }
+            });
           }
-        };
-      });
+        }
+      };
     }
 
 
--- pub/System/GoogleMapsPlugin/pkg.uncompressed.js
+++ pub/System/GoogleMapsPlugin/pkg.uncompressed.js
@@ -2564,50 +2564,50 @@ jQuery(function($) {
 
     if (typeof(opts.marker) !== 'undefined') {
 
-        opts.marker.events =  {
-          "click": function(marker,event,context) {
-            var $this = $(this),
-                infowindow = $this.gmap3({get:{name:"infowindow"}}),
-                content;
-
-            if (context.data) {
-              content = context.data.replace(/, */g, '<br />');
-      
-              if (infowindow) {
-                infowindow.open($this.gmap3("get"), marker);
-                infowindow.setContent(content);
-              } else {
-                $this.gmap3({
-                  infowindow: {
-                    anchor:marker, 
-                    options:{ content: content }
-                  }
-                });
-              }
+      opts.marker.events =  {
+        "click": function(marker, event, context) {
+          var $this = $(this),
+              infowindow = $this.gmap3({get:{name:"infowindow"}}),
+              content;
+          
+          if (context.data) {
+            content = context.data.replace(/, */g, '<br />');
+    
+            if (infowindow) {
+              infowindow.open($this.gmap3("get"), marker);
+              infowindow.setContent(content);
             } else {
               $this.gmap3({
-                getaddress: {
-                  latLng:marker.getPosition(),
-                  callback:function(results){
-                    var content = results && results[0] ? results[0].formatted_address : "no address";
-
-                    if (infowindow){
-                      infowindow.open($this.gmap3("get"), marker);
-                      infowindow.setContent(content);
-                    } else {
-                      $this.gmap3({
-                        infowindow: {
-                          anchor:marker, 
-                          options:{ content: content }
-                        }
-                      });
-                    }
-                  }
+                infowindow: {
+                  anchor:marker, 
+                  options:{ content: content }
                 }
               });
             }
+          } else {
+            $this.gmap3({
+              getaddress: {
+                latLng:marker.getPosition(),
+                callback:function(results){
+                  var content = results && results[0] ? results[0].formatted_address : "no address";
+
+                  if (infowindow){
+                    infowindow.open($this.gmap3("get"), marker);
+                    infowindow.setContent(content);
+                  } else {
+                    $this.gmap3({
+                      infowindow: {
+                        anchor:marker, 
+                        options:{ content: content }
+                      }
+                    });
+                  }
+                }
+              }
+            });
           }
-        };
+        }
+      };
     }
 
 
